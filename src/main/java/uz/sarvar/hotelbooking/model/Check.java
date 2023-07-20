190c9d97e0acdfe1b346676701fb5887dfb6aeb7
Sarvar <85160631+Abdivaliev@users.noreply.github.com>
2023-07-20 11:51:37 +0500

    ok

---
+++ src/main/java/uz/sarvar/hotelbooking/model/Check.java
@@ -0,0 +1,22 @@
+package uz.sarvar.hotelbooking.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Check {
+    private Integer price;
+    private Timestamp startDate;
+    private Timestamp endDate;
+    private String clientName;
+    private List<Room> rooms;
+    private String extraInfo;
+}
